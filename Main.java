3170544c116a74c08171e184b6d4758ade8f4f3d
LiliMeng <lilimeng1103@gmail.com>
2015-10-25 13:05:15 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main()
 	{
-		int numberOfUnits[]= {2,4,1};
+		int numOfUnitsInEachLayer[]= {2,4,1};
 		
 		//XOR training input
 		double totalTrainingInput[][] = { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 } };
@@ -12,13 +12,13 @@ public class Main {
 		//XOR training output
 		double outputTrainingSets[][] ={{0},{1},{1},{1}};
 		
-		double learningRate=0.2;
-		double momentum=0.9;
+		double learningRate= 0.2;
+		double momentum= 0.0;
 		double minimumError = 0.05;
 		long maxIterations = 10000;
 	 
-	    ErrorBackPropagation BP;
-	    BP(numberOfUnits, totalTrainingInput, outputTrainingSets,learningRate, momentum, minimumError, maxIterations).calculateOverallError;
-	
+	    ErrorBackPropagation  BP = new ErrorBackPropagation(numOfUnitsInEachLayer, totalTrainingInput, outputTrainingSets,learningRate, momentum, minimumError, maxIterations);
+	    BP.runBP();
+	    
 	}
 }
