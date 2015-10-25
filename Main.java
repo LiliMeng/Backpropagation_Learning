05c39bb8cfbddd824bee5a516175cccb6f43bab6
LiliMeng <lilimeng1103@gmail.com>
2015-10-24 22:30:51 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,9 +4,14 @@ public class Main {
 
 	public static void main()
 	{
-		int numberOfUnits[]= {2,3,3,3,3,1};
+		int numberOfUnits[]= {2,4,1};
+		
+		//XOR training input
 		double totalTrainingInput[][] = { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 } };
+		
+		//XOR training output
 		double outputTrainingSets[][] ={{0},{1},{1},{1}};
+		
 		double learningRate=0.2;
 		double momentum=0.9;
 		double minimumError = 0.05;
