0a51cb02badd03c2d2bd7bc6b4553f6833e98041
LiliMeng <lilimeng1103@gmail.com>
2015-10-27 17:01:32 -0700

    Update Unit.java

--- Unit.java
+++ Unit.java
@@ -3,61 +3,44 @@ package EECEBP;
 public class Unit {
 	
 	   // Constructor
-	   public Unit (int numberOfUnits) 
+	   public Unit (int numberOfInputUnits) 
 	    {
+	    
 	    	//Create an array of weight with the same size as the vector of inputs to the unit
-	    	weight = new double[numberOfUnits];
+	    	weight = new double[numberOfInputUnits];
 	    	
-	    	weightDiff = new double[numberOfUnits];
+	    	weightDiff = new double[numberOfInputUnits];
 	    	
 	    	//Initialize the weights and threshold to the node
 	    	initializeWeights();
 	    	
 	    }
-
-		//Output signal from current unit
-	    public double output;
 		
 	    //vector of weights from previous units to current unit
 	    public double weight[];
 
-		//unit bias
-	    public double bias = 1.0;
-	    
-	    //unit bias weight
-	    public double biasWeight;
-
-        //weight difference between the (n-1)th and nth iteration
+	    //weight difference between the (n-1)th and nth iteration
 	    public double weightDiff[];
-
-		//bias difference between the (n-1)th and nth iteration
-	    public double biasDiff;
 	    
-	    //Output signal error
+	    public double output;
+	    
 	    public double signalError;
-
+	
         //Initialize both bias and unit weights to random values in the range -0.5 to +0.5
 	    private void initializeWeights(){
-	    	
-	    	//Initialize the bias units with a random number between -0.5 and 0.5
-	    	biasWeight = 0.5*(-1+2*Math.random());
-	    	
-	    	bias=bias*biasWeight;
-	    	
-	    	//Initially, thresholdDiff is assigned to 0 so that the momentum term
-	    	//can work during the 1st iteration
-	    	biasDiff = 0;
 	    	
 	    	for(int i=0; i<weight.length; i++)
 	    	{
 	    		//Initialize weights to random values in the range -0.5 to +0.5
-	    		weight[i] = 0.5*(-1+2*Math.random());
+	    		weight[i] = Math.random()-0.5;
 	    		
 	    		//Initially, weightDiff is assigned to 0 so that the momentum term
 		    	//can work during the 1st iteration
 	    		weightDiff[i] = 0;
-	    	}  			
+	    	}  	
+
 	    }
 };
+
 
 
